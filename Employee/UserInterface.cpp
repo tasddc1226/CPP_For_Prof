d244b252d15f9a9438f367d19da97570620abc14
tasddc@naver.com <tasddc@naver.com>
2021-08-12 09:07:37 +0900

    doDemote 함수 추가

--- Employee/UserInterface.cpp
+++ Employee/UserInterface.cpp
@@ -52,73 +52,97 @@ int main()
 
 int displayMenu()
 {
-    // 화면에 메뉴를 출력 -> 사용자의 입력을 받음.
-    // 숫자를 요청하면 숫자를 입력한다고 가정.
-    // 잘못된 입력이 들어오면 예외 처리
-    int selection;
+	// 화면에 메뉴를 출력 -> 사용자의 입력을 받음.
+	// 숫자를 요청하면 숫자를 입력한다고 가정.
+	// 잘못된 입력이 들어오면 예외 처리
+	int selection;
 
-    cout << endl;
-    cout << "Employee Database" << endl;
-    cout << "-----------------" << endl;
-    cout << "1) Hire a new employee" << endl;
-    cout << "2) Fire an employee" << endl;
-    cout << "3) Promote an employee" << endl;
-    cout << "4) List all employees" << endl;
-    cout << "5) List all current employees" << endl;
-    cout << "6) List all former employees" << endl;
-    cout << "0) Quit" << endl;
-    cout << endl;
-    cout << "---> ";
-    
+	cout << endl;
+	cout << "Employee Database" << endl;
+	cout << "-----------------" << endl;
+	cout << "1) Hire a new employee" << endl;
+	cout << "2) Fire an employee" << endl;
+	cout << "3) Promote an employee" << endl;
+	cout << "4) List all employees" << endl;
+	cout << "5) List all current employees" << endl;
+	cout << "6) List all former employees" << endl;
+	cout << "0) Quit" << endl;
+	cout << endl;
+	cout << "---> ";
+
 	cin >> selection;
-    
+
 	return selection;
 }
 
 void doHire(Database& db)
 {
-    // 신입직원의 이름을 입력받아 DB에 그 직원에 대한 정보 추가요청
-    string firstName;
-    string lastName;
+	// 신입직원의 이름을 입력받아 DB에 그 직원에 대한 정보 추가요청
+	string firstName;
+	string lastName;
 
-    cout << "First name? ";
-    cin >> firstName;
-    cout << "Last name? ";
-    cin >> lastName;
-    
-    db.addEmployee(firstName, lastName);
+	cout << "First name? ";
+	cin >> firstName;
+	cout << "Last name? ";
+	cin >> lastName;
+
+	db.addEmployee(firstName, lastName);
 }
 
 void doFire(Database& db)
 {
-    int employeeNumber;
+	int employeeNumber;
 
-    cout << "Employee number? ";
-    cin >> employeeNumber;
+	cout << "Employee number? ";
+	cin >> employeeNumber;
 
-    try {
-        Employee& emp = db.getEmployee(employeeNumber);
-        emp.fire();
-        cout << "Employee " << employeeNumber << " terminated." << endl;
-    } catch (const std::logic_error& exception) {
-        cerr << "Unable to terminate employee: " << exception.what() << endl;
-    }
+	try {
+		Employee& emp = db.getEmployee(employeeNumber);
+		emp.fire();
+		cout << "Employee " << employeeNumber << " terminated." << endl;
+	}
+	catch (const std::logic_error& exception) {
+		cerr << "Unable to terminate employee: " << exception.what() << endl;
+	}
 }
 
 void doPromote(Database& db)
 {
-    int employeeNumber;
-    int raiseAmount;
+	int employeeNumber;
+	int raiseAmount;
 
-    cout << "Employee number? ";
-    cin >> employeeNumber;
-    cout << "How much of a raise? ";
-    cin >> raiseAmount;
+	cout << "Employee number? ";
+	cin >> employeeNumber;
+	cout << "How much of a raise? ";
+	cin >> raiseAmount;
 
-    try {
-        Employee& emp = db.getEmployee(employeeNumber);
-        emp.promote(raiseAmount);
-    } catch (const std::logic_error& exception) {
-        cerr << "Unable to promote employee: " << exception.what() << endl;
-    }
+	try {
+		Employee& emp = db.getEmployee(employeeNumber);
+		emp.promote(raiseAmount);
+	}
+	catch (const std::logic_error& exception) {
+		cerr << "Unable to promote employee: " << exception.what() << endl;
+	}
+}
+
+void doDemote(Database& db) {
+	int employeeNumber;
+	int demeritAmount;
+
+	cout << "Employee number? ";
+	cin >> employeeNumber;
+	cout << "How much of a demerit? ";
+	cin >> demeritAmount;
+
+	try
+	{
+		Employee& emp = db.getEmployee(employeeNumber);
+		emp.demote(demeritAmount);
+	}
+	catch (const std::logic_error& exception)
+	{
+		cerr << "Unable to demote employee. " << exception.what() << endl;
+
+	}
+
 }
